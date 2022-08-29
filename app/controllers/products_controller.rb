ba8ff32af87db19d3be38d952385404373eb89e6
rikakohira <rikako.takase88@gmail.com>
2022-08-29 12:21:04 +0900

    コントローラ 設定

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,24 @@
+class ProductsController < ApplicationController
+  def new
+    @form = Form::ProductCollection.new
+  end
+
+  def create
+    @form = Form::ProductCollection.new(product_collection_params)
+    if @form.save
+      redirect_to products_path, notice: "商品を登録しました"
+    else
+      flash.now[:alert] = "商品登録に失敗しました"
+      render :new
+    end
+
+  end
+
+  private
+
+    def product_collection_params
+        params.require(:form_product_collection)
+        .permit(products_attributes: [:name, :price, :unit, :availability])
+    end
+  
+end
